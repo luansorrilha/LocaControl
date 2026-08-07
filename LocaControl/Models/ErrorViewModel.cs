8627ef55b0cda71aee0d9b29d36b1d47790280ce
Luan Samuel Sorrilha Leao <luansorrilha21@gmail.com>
2026-08-07 00:04:19 -0300

    Adicionar arquivos de projeto.

---
+++ LocaControl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocaControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
